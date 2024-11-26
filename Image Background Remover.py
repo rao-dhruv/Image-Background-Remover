d355c5fa12041a2696b460d4122a630624cb4b17
Dhruv Rao <57631631+rao-dhruv@users.noreply.github.com>
2024-11-26 07:15:18 -0500

    Update Image Background Remover.py

--- Image Background Remover.py
+++ Image Background Remover.py
@@ -1,9 +1,9 @@
 from rembg import remove
 from PIL import Image
 
-input_image = "/Users/dhruvrao/Desktop/uconn_huskies_logo.jpg" #Your input Image path/name
-output_image = "/Users/dhruvrao/Desktop/rmbg_image.png" #Output Image path/name
+input_image = "uconn_huskies_logo.jpg" #Your input Image path/name
+output_image = "rmbg_image.png" #Output Image path/name
 img = Image.open(input_image)
 output = remove(img)
 output.save(output_image)
-Image.open("/Users/dhruvrao/Desktop/rmbg_image.png").show()
+Image.open("rmbg_image.png").show()
